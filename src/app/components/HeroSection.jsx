cc7379e573f2f4f021603cd8b8fbd687374b4607
Faheem Abdulla <faheemkr7@gmail.com>
2026-07-03 07:29:01 +0530

    twentieth commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -130,7 +130,7 @@ const HeroSection = () => {
             </Link>
             <Link
              target="_blank"
-              href="https://drive.google.com/file/d/1RMvcaDCM5Q78c2gvlf3c1tnwuLNPY-8J/view?usp=sharing"
+              href="https://drive.google.com/file/d/1mLg78iZGU_8QCPBkqtlmth0JizQjtazG/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
